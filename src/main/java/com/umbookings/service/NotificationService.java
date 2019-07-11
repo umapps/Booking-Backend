cbf92e124af4f141c8246a3b4c99f3642f3fbdfe
Shrikar Kalagi <shrikarvk@gmail.com>
2019-07-11 16:22:21 +0530

    Use AWS SDK classes to send Email and SMS

--- src/main/java/com/umbookings/service/NotificationService.java
+++ src/main/java/com/umbookings/service/NotificationService.java
@@ -1,20 +1,11 @@
 package com.umbookings.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
@@ -26,10 +17,6 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
-import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
-import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
-import com.amazonaws.services.simplesystemsmanagement.model.GetParametersRequest;
-import com.amazonaws.services.simplesystemsmanagement.model.GetParametersResult;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
@@ -39,15 +26,7 @@ import com.amazonaws.services.sns.model.PublishResult;
  */
 @Service
 public class NotificationService {
-	
-//	@Value("${aws.access.key.id}")
-//	private String awsAccessKey;
-//	
-//	@Value("${aws.secret.access.key}")
-//	private String awsSecretAccessKey;
-//	
-//	@Value("${spring.email.sender}")
-//	private String fromEmail;
+
 	
 	@Inject
 	private ConfigurableEnvironment environment;
@@ -57,7 +36,7 @@ public class NotificationService {
 	public String sendEmail(String emailId) {
 		try {
 			
-			AmazonSimpleEmailService client = getAWSCredentials();
+			AmazonSimpleEmailService emailClient = getAWSCredentials();
 		        SendEmailRequest request = new SendEmailRequest()
 		            .withDestination(
 		                new Destination().withToAddresses(emailId))
@@ -70,7 +49,7 @@ public class NotificationService {
 		                .withSubject(new Content()
 		                    .withCharset("UTF-8").withData("Test email from UM-Apps")))
 		            .withSource("shrikarvk@gmail.com");
-		        client.sendEmail(request);
+		        emailClient.sendEmail(request);
 			LOG.info("Email sent successfully to {}" ,emailId);
 			return "Email sent successfully to " +emailId;
 		} catch (Exception e) {
@@ -81,7 +60,7 @@ public class NotificationService {
 
 	private AmazonSimpleEmailService getAWSCredentials() {
 		AmazonSimpleEmailService client = null;
-		if (environment.getProperty("aws.access.key.id") != null && environment.getProperty("aws.secret.access.key") != null)
+		if (!(environment.getProperty("aws.access.key.id").equals("") && environment.getProperty("aws.secret.access.key").equals("")))
 		
 			{
 			BasicAWSCredentials awsCreds = new BasicAWSCredentials(environment.getProperty("aws.access.key.id"),
@@ -100,18 +79,27 @@ public class NotificationService {
 	
 	@SuppressWarnings("deprecation")
 	public String sendSMS(String mobileNumber) {
+		AmazonSNSClient snsClient = null;
 		try {
+			if(!(environment.getProperty("aws.access.key.id").equals("") && environment.getProperty("aws.secret.access.key").equals("")))
+			{
+				BasicAWSCredentials awsCreds = new BasicAWSCredentials(environment.getProperty("aws.access.key.id"),
+						environment.getProperty("aws.secret.access.key"));
+	         snsClient = new AmazonSNSClient(awsCreds);
+			}
+			else
+			{
+				 snsClient = new AmazonSNSClient();
+			}
 
-//	    AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretAccessKey);
-//        AmazonSNSClient snsClient = new AmazonSNSClient();
-//        snsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
-//        String message = "Test Message from UM Apps";
-//        String phoneNumber = "+91"+mobileNumber;
-//        PublishResult result = snsClient.publish(new PublishRequest()
-//                .withMessage(message)
-//                .withPhoneNumber(phoneNumber)
-//                );       
-//        LOG.info("SMS sent successfully to {} with message id {}" ,mobileNumber, result);
+        snsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
+        String message = "Test Message from UM Apps";
+        String phoneNumber = "+91"+mobileNumber;
+        PublishResult result = snsClient.publish(new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(phoneNumber)
+                );       
+        LOG.info("SMS sent successfully to {} with message id {}" ,mobileNumber, result);
         return "SMS sent successfully with message id ";
 		}
 		catch(Exception e)
