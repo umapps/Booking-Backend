ea39269e575d3b89bbfa597ef2c8dd7a536dc0e4
Shrikar Kalagi <shrikarvk@gmail.com>
2019-07-16 09:58:24 +0530

    Correct file path

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 chmod +x /home/ec2-umbookings/umbookings-backend-0.0.1-SNAPSHOT.jar
 chmod +x /home/ec2-umbookings/start_server.sh
-chmod +x /home/ec2-umbookings/stop_server.sh
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/ec2-user/umbookings-backend-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-umbookings/umbookings-backend-0.0.1-SNAPSHOT.jar
