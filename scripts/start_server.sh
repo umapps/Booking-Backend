261f90fe50191de4f0368e205007eaa33fe676a8
Shrikar Kalagi <shrikarvk@gmail.com>
2019-07-16 12:02:39 +0530

    Add more params for server start

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/ec2-umbookings/umbookings-backend-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-umbookings/umbookings-backend-0.0.1-SNAPSHOT.jar /dev/null 2> /dev/null < /dev/null &
